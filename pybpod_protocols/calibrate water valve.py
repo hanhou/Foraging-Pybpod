4ed859acf5df8040bba513dde09f9352337fd974
Márton Rózsa <rozmar@users.noreply.github.com>
2020-02-07 19:57:07 -0500

    3lp

--- pybpod_protocols/calibrate water valve.py
+++ pybpod_protocols/calibrate water valve.py
@@ -12,10 +12,12 @@ from pybpodapi.bpod.hardware.output_channels import OutputChannel
 import timeit
 
 
-valvetimes = [.1,.02,.025,.03,.035,.04]
+valvetimes = [.03,.04,.05,.06,.07]
 Dropnum = 5
 ValveOpenTime_L = 0.1#ds
 ValveOpenTime_R = 0.1#nds
+ValveOpenTime_M = 0.1#nds
+waittime = 2
 ValveCloseTime = 3
 
 my_bpod = Bpod()
@@ -27,6 +29,7 @@ for valvetime in valvetimes:
     ValveOpenTime_L = valvetime
 
     ValveOpenTime_R = valvetime
+    ValveOpenTime_M = valvetime
 
     print(ValveOpenTime_R)
     for i in range(Dropnum):  # Main loop
@@ -41,23 +44,33 @@ for valvetime in valvetimes:
 	    sma.add_state(
 		state_name='Open Left',
 		state_timer=ValveOpenTime_L,
-		state_change_conditions={EventName.Tup: 'Open Left 2'},
+		state_change_conditions={EventName.Tup: 'wait1'},
 		output_actions = [('Valve',1)])
 	    sma.add_state(
-		state_name='Open Left 2',
-		state_timer=ValveOpenTime_L,
+		state_name='wait1',
+		state_timer=waittime,
 		state_change_conditions={EventName.Tup: 'Open Right'},
-		output_actions = [('Valve',7)])
+		output_actions = [])
 	    sma.add_state(
 		state_name='Open Right',
 		state_timer=ValveOpenTime_R,
-		state_change_conditions={EventName.Tup: 'Open Right 2'},
+		state_change_conditions={EventName.Tup: 'wait2'},
 		output_actions = [('Valve',2)])
 	    sma.add_state(
-		state_name='Open Right 2',
-		state_timer=ValveOpenTime_R,
+		state_name='wait2',
+		state_timer=waittime,
+		state_change_conditions={EventName.Tup: 'Open Middle'},
+		output_actions = [])
+	     sma.add_state(
+		state_name='Open Middle',
+		state_timer=ValveOpenTime_M,
+		state_change_conditions={EventName.Tup: 'wait2'},
+		output_actions = [('Valve',3)])
+	    sma.add_state(
+		state_name='wait2',
+		state_timer=waittime,
 		state_change_conditions={EventName.Tup: 'Close Valves'},
-		output_actions = [('Valve',8)])
+		output_actions = [])
 	    if i == 0:
 		    sma.add_state(
 			state_name = 'Close Valves',
