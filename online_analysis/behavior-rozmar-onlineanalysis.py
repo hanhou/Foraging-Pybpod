8543e1a7b44e291d74c885e007262a600a0bf2c2
han-rig2-new <houhan@gmail.com>
2021-03-22 21:38:16 -0400

    change 'is' to '=='

--- online_analysis/behavior-rozmar-onlineanalysis.py
+++ online_analysis/behavior-rozmar-onlineanalysis.py
@@ -411,11 +411,11 @@ class App(QDialog):
                 # needed = days[key] == np.datetime64(session)
                 same_day_this_session = times[key][days[key] == np.datetime64(session)]
                 
-                earliest_start_cutoff = 5  # The session will not start earlier than 5AM
+                earliest_start_cutoff = -1  # The session will not start earlier than 5AM
                 longest_session = 6  # Longest session = 6 hours
                 if len(same_day_this_session):
                     # Take care of the situation where one training session crosses two days...
-                    if key is 'alltimes':
+                    if key == 'alltimes':
                         start_time_this_session = same_day_this_session[pd.DatetimeIndex(same_day_this_session).hour > earliest_start_cutoff][0]
                         
                     needed = np.logical_and(times[key] >= start_time_this_session, (times[key] - start_time_this_session) < np.timedelta64(longest_session,'h'))
