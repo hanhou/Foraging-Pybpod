6fb080218d5ae7a3a223f1fc668892b4734924f1
han-ephys-behavior <houhan@gmail.com>
2022-05-17 19:22:41 -0700

    <pybpod> update calibrate_water_valve.py

--- pybpod_protocols/calibrate_water_valve.py
+++ pybpod_protocols/calibrate_water_valve.py
@@ -11,17 +11,27 @@ from pybpodapi.bpod.hardware.events import EventName
 from pybpodapi.bpod.hardware.output_channels import OutputChannel
 
 #############################################################
-water_ch = 3
+water_ch = 2	
 
-valvetime = 0.1
-dropnum = 100
-iti = 0.3
+valvetime = 0.035
+dropnum = 300
+iti = 0.5
 
+if_use_analog_module = 1
 
-sound_ch = OutputChannel.PWM5
+if if_use_analog_module:
+    SER_DEVICE = OutputChannel.Serial1
+    SER_CMD_GO_CUE = 1 
+    WAV_PORTS_SPEAKER = 1
+    WAV_ID_GO_CUE = 0
+    goCue_command = (SER_DEVICE, SER_CMD_GO_CUE)    # Use Wav ePlayer serial command #SER_CMD_GO_CUE on ephys rig!! 
+else:
+    goCue_command = ( OutputChannel.PWM5, 255)  # Set PWM5 to 100% duty cycle (always on), which triggers the wav trigger board
+
 ############################################################
 
 my_bpod = Bpod()
+my_bpod.load_serial_message(int(SER_DEVICE[-1]), SER_CMD_GO_CUE, [ord('P'), WAV_PORTS_SPEAKER, WAV_ID_GO_CUE])  # go cue
 
 for i in range(dropnum):  # Main loop
     print('Trial: ', i + 1)
@@ -39,16 +49,21 @@ for i in range(dropnum):  # Main loop
         		state_name='ITI',
         		state_timer=iti,
         		state_change_conditions={EventName.Tup: 'exit'},
-        		output_actions = [(sound_ch, 255)])
+        		output_actions = ([goCue_command]))
     else:
         sma.add_state(
         		state_name='ITI',
         		state_timer=iti,
         		state_change_conditions={EventName.Tup: 'exit'},
         		output_actions = [])
-        
-    my_bpod.send_state_machine(sma)  # Send state machine description to Bpod device
-    my_bpod.run_state_machine(sma)  # Run state machine
+
+    while True:
+        try:
+            my_bpod.send_state_machine(sma)  # Send state machine description to Bpod device
+            my_bpod.run_state_machine(sma)  # Run state machine
+            break
+        except:
+            pass
     
     
 my_bpod.close()
